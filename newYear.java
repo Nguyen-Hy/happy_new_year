2b7b54665f5e53551b89bb3d2e60f6deaea87204
Nguyen Hy <88277877+Nguyen-Hy@users.noreply.github.com>
2022-01-01 14:25:01 +0700

    Add files via upload

---
+++ newYear.java
@@ -0,0 +1,17 @@
+package Test;
+
+public class newYear {
+
+    public static void main(String[] args) {
+
+        var year = 2021;
+        while( year == 2021 ){
+            year++;
+            System.out.println("\n\t"+year);
+        }
+
+        System.out.println("\tHappy New Year");
+
+    }
+
+}
